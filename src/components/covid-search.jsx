1da64b0acb49648bdbed8eacd0a8ce46ce7d1fbc
frankjlin16 <flin30@asu.edu>
2021-08-17 17:48:02 -0700

    Finished search and country undefined handling

--- src/components/covid-search.jsx
+++ src/components/covid-search.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 
 class CovidSearch extends Component {
-  state = {};
+  state = {
+    value: "",
+  };
+
+  handleChange = (event) => {
+      this.setState({value: event.target.value})
+  }
+
+  handleSearchSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.value);
+  };
+
+
   render() {
     return (
       <div className="container">
-        <form className="d-flex">
+        <form className="d-flex" onSubmit={this.handleSearchSubmit}>
           <input
             className="form-control me-2"
-            type="text"
+            type="search"
             placeholder="Search"
             aria-label="Search"
+            value={this.state.value}
+            onChange={this.handleChange}
           />
-          <button className="btn btn-outline-success">
-            Search
-          </button>
+          <input type="submit" className="btn btn-outline-success" />
         </form>
       </div>
     );
